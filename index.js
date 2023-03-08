57c74e77aa687cd18f19425564e230ecf9c3cb38
tianxing <tianxing@codemao.cn>
2023-03-08 20:28:01 +0800

    chore: 统一规范配置

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 120, // 一行多少个字符
+  tabWidth: 2, // 一个tab代表几个空格数
+  useTabs: false, // 用空格进行缩进
+  semi: false, // 行尾不使用分号
+  singleQuote: true, // 使用单引号
+  quoteProps: 'as-needed', // 对象属性仅有需要时添加引号
+  jsxSingleQuote: false, // JSX使用双引号
+  trailingComma: 'es5', // 尾随逗号，按ES5标准
+  bracketSpacing: true, // 对象大括号有空格，效果：{ foo: bar }
+  bracketLine: false, // 多行html元素时“>”单独占一行
+  arrowParens: 'always', // 箭头函数参数始终加上括号
+  proseWrap: 'preserve', // markdown保持原样
+  endOfLine: 'lf', // 换行符使用“lf”
+}
